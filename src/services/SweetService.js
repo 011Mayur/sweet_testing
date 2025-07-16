e702ec2410d6735c2d197d217bcce4e16aaaaab2
Panchal Mayur <panchalmayur052@gmail.com>
2025-07-16 16:49:03 +0530

    🟢 GREEN: Prevent adding duplicate sweets by ID

--- src/services/SweetService.js
+++ src/services/SweetService.js
@@ -4,8 +4,12 @@ class SweetService {
   }
 
   addSweet(sweet) {
-    this.sweets.push(sweet);
+  const exists = this.sweets.find(s => s.id === sweet.id);
+  if (exists) {
+    throw new Error("Sweet ID must be unique");
   }
+  this.sweets.push(sweet);
+}
 
   getAllSweets() {
     return this.sweets;
